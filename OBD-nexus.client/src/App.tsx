1295bc72ffb1d8b1e0e3816d8114caf923861308
ThomasMumladze <98337658+ThomasMumladze@users.noreply.github.com>
2026-09-21 22:40:58 +0400

    fixed header position and app style

--- OBD-nexus.client/src/App.tsx
+++ OBD-nexus.client/src/App.tsx
@@ -4,7 +4,12 @@ import Header from "./layout/Header";
 function App() {
     return (
         <div className="app">
-            <Header />
+            <div className="app--content">
+                <Header />
+                <div className="app--content__inner">
+                    <div style={{ height: "10230px" }}></div>
+                </div>
+            </div>
         </div>
     );
 }
